4733b1e607a0c06de6cd9a5a74ca5113a576147e
Tevin Padayachee <tevinp1@icloud.com>
2022-05-18 23:31:42 +0200

    Revert "Vote button stage 1"
    This reverts commit 13cc5ece1b79559429fb7846e646fd0166b21370.

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -4,10 +4,8 @@ export default {
     name : 'Home',
     data(){
         return{
-            voter:0,
-          
             questions : [
-                {   voteid:0,
+                {
                     votes : 12,
                     answers : 2,
                     heading :'How to center a div',
@@ -17,7 +15,7 @@ export default {
                     tags : 'php,html,css',
                     id : 1
                 },
-                {   voteid:1,
+                {
                     votes : 2,
                     answers : 28,
                     heading :'Hello world problem',
@@ -28,7 +26,6 @@ export default {
                     id : 2
                 },
                 {
-                    voteid:2,
                     votes : 32,
                     answers : 344,
                     heading :'yii module not working',
@@ -37,11 +34,8 @@ export default {
                     user : 'Johnathan',
                     tags : 'php,html,css',
                     id : 3
-                },
-                            
-            ],
-           
-            
+                }                
+            ]
         }
     },
     methods:{
@@ -70,15 +64,8 @@ export default {
              })
 
                 
-        },
-     
-          
-       
-        
+        }
 
-    },
-    compute:{
-      
     },
     mounted(){
         this.testCall()
@@ -95,7 +82,7 @@ export default {
                 <a v-for="question in questions" :key="question.id" class="list-group-item list-group-item-action d-flex flex-row">
                     <div class="buttons-container d-flex flex-column col-1">
                         <div class="votes-container">
-                                <p>{{voter}}</p>
+                                <p>{{question.votes}}</p>
                                 <p>votes</p>
 
                         </div>
@@ -116,13 +103,6 @@ export default {
                     <div class="d-flex flex-column col justify-content-between">
                         <small>{{question.user}}</small>
                         <small>{{question.time}}</small>
-                        
-                      
-                              <button @click="voter++">Upvote</button>
-                       
-                            
-                        
-                        
                     </div>
 
                 </a>
@@ -147,8 +127,6 @@ export default {
                         <div class="votes-container">
                                 <p>{{question.votes}}</p>
                                 <p>votes</p>
-                                
-                                
 
                         </div>
 
