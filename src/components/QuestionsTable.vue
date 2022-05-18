8a4d1d366defdc3b7a03f12a818084c2df585d1f
Tevin Padayachee <tevinp1@icloud.com>
2022-05-19 00:26:10 +0200

    changed questionslist layout

--- src/components/QuestionsTable.vue
+++ src/components/QuestionsTable.vue
@@ -1,95 +1,154 @@
 <script>
+import axios from 'axios'
+export default {
+    name : 'QuestionsTable',
+    data(){
+        return{
+            questions : [             
+            ]
+        }
+    },
+    methods:{
+         testCall(){
+             axios.get('http://localhost:4000/questions/all')
+             .then(response =>{
+                 console.log(response.data['result'])
+                 this.questions = response.data['result']
+             })
 
+                
+        }
+
+    },
+    mounted(){
+        this.testCall()
+    }
+}
 </script>
 
-
-
 <template>
+        <div class="top-questions-container d-flex flex-column">
+            <h3>All Questions</h3>
 
-    <div class="table-data">
-      
-      <table class="content-table" border="1" frame="void" rules="rows">
-        <tr>
-          <th>Heading</th>
-          <th>Description</th>
-          <th>Users</th>
-          <th>Time</th>
-        </tr>
+            <div class="list-group">
 
-        &lt% if(userData.length!=0){ var i=1; userData.forEach(function(result){ %>
-        <tr>
-          <td>&lt%=result.heading %></td>
-          <td>&lt%=result.description %></td>
-          <td>&lt%=result.user_id %></td>
-          <td>&lt%=result.time %></td>
-          <td>
-            <a href=""
-              >View All Answers To this Question
+                <a v-for="question in questions" :key="question.id" class="list-group-item list-group-item-action d-flex flex-row">
+                    <div class="buttons-container d-flex flex-column col-1">
+                        <div class="votes-container">
+                                <p>{{question.votes}}</p>
+                                <p>votes</p>
+
+                        </div>
+
+                        <div class="answers-container">
+                            <p>{{question.answers}}</p>
+                            <p>answers</p>
+                            
+                        </div>
+                    </div>
+
+                    <div class="d-flex d-flex flex-column col-9">
+                        <h5 class="mb-1">{{question.heading}}</h5>
+                        <p class="mb-1">{{question.description}}</p>
+                        <p class="mb-1">{{question.tags}}</p>
+                    </div>
+
+                    <div class="d-flex flex-column col justify-content-between">
+                        <small>{{question.user}}</small>
+                        <small>{{question.time}}</small>
+                    </div>
+
+                </a>
+
+            </div>
+
+        </div>
+
+        <div class="d-flex function-buttons-container">
+            <a class="btn-ask-question" href="#/signin">
+                Ask a question
             </a>
-          </td>
-        </tr>
-        &lt% i++; }) %> &lt% } else{ %>
-        <tr>
-          <td colspan="7">No Data Found</td>
-        </tr>
-        &lt% } %>
-      </table>
+            <a class="btn btn-outline-dark" href="#/signin">
+                Vote
+            </a>       
+        </div>
 
-    
-      <a class="nav-item nav-link active" href="">Ask A Question</a>
-    </div>
 </template>
 
-
-
-
-
 <style scoped>
-  * {
-      font-family: sans-serif; /* Change your font family */
-    }
-
-    .content-table {
-      border-collapse: collapse;
-      margin: 25px 0;
-      font-size: 0.9em;
-      min-width: 100%;
-      border-spacing: 100px;
-      border-radius: 5px 5px 0 0;
-      overflow: hidden;
-      box-shadow: 0 0 20px #007bff;
-      text-align: left;
-    }
-
-    .content-table thead tr {
-      background-color: #007bff;
-      color: #007bff;
-      text-align: center;
-      font-weight: bold;
-    }
-
-    .content-table th,
-    .content-table td {
-      padding: 12px 15px;
-    }
-
-    .content-table tbody tr {
-      border-bottom: 1px solid #007bff;
-    }
-
-    .content-table tbody tr:nth-of-type(even) {
-      background-color: #d3d3d3;
-    }
-
-    .content-table tbody tr:last-of-type {
-      border-bottom: 2px solid #007bff;
-    }
-
-    .content-table tbody tr.active-row {
-      font-weight: bold;
-      color: #007bff;
-    }
+.top-questions-container{
+    margin: 50px auto;
+    width: 90%;
+}
 
 
+.list-group{
+    width: 100%;
+    margin: 30px auto;
+}
 
+.list-group a{
+    padding: 15px;
+}
+
+.buttons-container{
+    margin-right: 40px;
+    justify-content: center;
+    align-items: center;
+}
+
+.votes-container{
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    color: hsl(210,8%,45%);
+    border-top-left-radius: 6px;
+    border-top-right-radius: 6px;
+    text-align: center;
+    width: 90px;
+}
+
+.answers-container{
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    background: hsl(140,40%,55%);
+    border-radius: 6px;
+    text-align: center;
+    width: 90px;
+}
+
+.answers-container p{
+    color: white;
+}
+
+
+.votes-container p, .answers-container p{
+    margin: 0;
+}
+
+.function-buttons-container{
+    justify-content: center;
+    align-items: center;
+}
+
+.function-buttons-container a{
+    margin: 10px;
+    border-radius: 4px;
+    text-decoration: none;
+    font-weight: 600;
+    width: 130px;
+    height: 50px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    padding: 7px;
+}
+
+.btn-ask-question{
+    background: #0d6efd;
+    color: white;
+}
 </style>
