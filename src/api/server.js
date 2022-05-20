51ad5ae90cf7a58c17b2bef545e97be49ee6e53a
Shahil-Harripersad <2094785@students.wits.ac.za>
2022-05-20 13:13:20 +0200

    Cloned branch from login-reg branch

--- src/api/server.js
+++ src/api/server.js
@@ -1,89 +1,166 @@
 // app.js
-const express = require('express')
+const express = require("express");
 
 //mysql
-const mysql = require('mysql');
+const mysql = require("mysql");
 
 //allows to interpret as json
-const bodyParser = require("body-parser"); 
+const bodyParser = require("body-parser");
 
 //cors
-const cors = require('cors');
+const cors = require("cors");
 
 //create connection
 const connection = mysql.createConnection({
-    host: "us-cdbr-east-05.cleardb.net",
-    user: "b4129b27e9a1e2",
-    password: "87009bd8",
-    database: "heroku_45ea15f427c56e8"
+  host: "us-cdbr-east-05.cleardb.net",
+  user: "b4129b27e9a1e2",
+  password: "87009bd8",
+  database: "heroku_45ea15f427c56e8",
 });
 
 // Create Express app
-const exp = express()
+const exp = express();
 
-exp.use(cors())
+exp.use(cors());
 
-exp.use(bodyParser.urlencoded(true))
-exp.use(bodyParser.json())
-exp.use(bodyParser.raw())
+exp.use(bodyParser.urlencoded(true));
+exp.use(bodyParser.json());
+exp.use(bodyParser.raw());
 
 //login with hashed password
-exp.post('/login', (req, res) => {
-    if(req.body.username != '' && req.body.password != ''){
-        connection.query("select UserPassword from registered_user where Username = ?", [req.body.username], function (err, results, fields) {
+exp.post("/login", (req, res) => {
+  if (req.body.username != "" && req.body.password != "") {
+    connection.query(
+      "select UserPassword from registered_user where Username = ?",
+      [req.body.username],
+      function (err, results, fields) {
         const isValid = bcrypt.compare(req.body.password, String(results[0]));
-        if(results != 0 && isValid){
-            res.send({status: "true"});
-            //res.end();
-        } else{
-            res.send({status:"false"})
+        if (results != 0 && isValid) {
+          res.send({ status: "true" });
+          //res.end();
+        } else {
+          res.send({ status: "false" });
         }
-    });
-    }
-    
-})
+      }
+    );
+  }
+});
 
 // questions to display on homepage
-exp.get('/questions', (req, res) => {
-    connection.query("select * from tbl_question", function (err, result, fields) {
-        if(result){
-            res.send({
-                result: result,
-                status: "true"
+exp.get("/questions", (req, res) => {
+  connection.query(
+    "select * from tbl_question",
+    function (err, result, fields) {
+      if (result) {
+        res.send({
+          result: result,
+          status: "true",
         });
-        } else{
-            res.send({status:"false"})
-        }
-    });
-})
+      } else {
+        res.send({ status: "false" });
+      }
+    }
+  );
+});
 
 const { Prohairesis } = require("prohairesis"); //for mysql
-const mySQLstring = 'mysql://b4129b27e9a1e2:87009bd8@us-cdbr-east-05.cleardb.net/heroku_45ea15f427c56e8?reconnect=true'
-const database = new Prohairesis(mySQLstring)
-exp.use(express.static('public'))
-const bcrypt = require('bcrypt');
-const { request } = require('http');
+const mySQLstring =
+  "mysql://b4129b27e9a1e2:87009bd8@us-cdbr-east-05.cleardb.net/heroku_45ea15f427c56e8?reconnect=true";
+const database = new Prohairesis(mySQLstring);
+exp.use(express.static("public"));
+const bcrypt = require("bcrypt");
+const { request } = require("http");
 
 //insert new user into table
-exp.post('/register', (req, res) => {
-    connection.query("select * from registered_user where Username = ?", [req.body.USERNAME],  function (err, result, fields) {
-        if(result.length > 0){
-            res.send({status: "fail"}); //username already exists
-        } else {
-            //hash password
-            const hashedPassword = bcrypt.hashSync(req.body.PASSWORD, bcrypt.genSaltSync());
+exp.post("/register", (req, res) => {
+  connection.query(
+    "select * from registered_user where Username = ?",
+    [req.body.USERNAME],
+    function (err, result, fields) {
+      if (result.length > 0) {
+        res.send({ status: "fail" }); //username already exists
+      } else {
+        //hash password
+        const hashedPassword = bcrypt.hashSync(
+          req.body.PASSWORD,
+          bcrypt.genSaltSync()
+        );
 
-            database.execute(
-            'INSERT INTO registered_user (Username, Email, UserPassword) VALUES (@USERNAME, @EMAIL, @PASSWORD)',
-            {
-                USERNAME: req.body.USERNAME,
-                EMAIL: req.body.EMAIL,
-                PASSWORD: hashedPassword,
-            });
-            res.send({status: "pass"}); //registration successfull
-        }
-    });
-})
+        database.execute(
+          "INSERT INTO registered_user (Username, Email, UserPassword) VALUES (@USERNAME, @EMAIL, @PASSWORD)",
+          {
+            USERNAME: req.body.USERNAME,
+            EMAIL: req.body.EMAIL,
+            PASSWORD: hashedPassword,
+          }
+        );
+        res.send({ status: "pass" }); //registration successfull
+      }
+    }
+  );
+});
+
+// set user details 2
+exp.post("/account/set2", (req, res) => {
+  connection.query(
+    "select * from register_user where Username = ? and UserPassword = ?",
+    [req.body.username, req.body.password],
+    function (err, result, fields) {
+      if (result) {
+        //res.send({ status: "true" });
+        database.execute(
+          "update table registered_user set UserPassword = @PASSWORD where Userid = @USERID",
+          {
+            PASSWORD: req.body.PASSWORD,
+            USERID: req.body.USERID,
+          }
+        );
+      } else {
+        res.send({ status: "false" });
+      }
+    }
+  );
+});
+
+exp.post("/account/set", (req, res) => {
+  connection.query(
+    "select * from registered_user where Userid=?",
+    [req.body.userId],
+    function (err, result, fields) {
+      if (result) {
+        database.execute(
+          "update table registered_user set Username = @USERNAME,UserPassword = @PASSWORD where Userid = @USERID",
+          {
+            USERNAME: req.body.USERNAME,
+            PASSWORD: req.body.PASSWORD,
+            USERID: req.body.USERID,
+          }
+        );
+        res.send({ status: "details updated" });
+      } else {
+        res.send({ status: "update failed" });
+      }
+    }
+  );
+});
+
+// get user data for account page
+exp.post("/account/get", (req, res) => {
+  connection.query(
+    "select * from registered_user where Userid = ?",
+    [req.body.userId],
+    function (err, result, fields) {
+      if (result) {
+        res.send({
+          result: result,
+          status: "true",
+        });
+      } else {
+        res.send({ status: "false" });
+      }
+    }
+  );
+});
 
 // Start the Express server
-exp.listen(4000, () => console.log('Server running on port 4000!'))
+exp.listen(4000, () => console.log("Server running on port 4000!"));
--- src/components/Account.vue
+++ src/components/Account.vue
@@ -1,200 +1,322 @@
 <script>
+import axios from "axios";
 export default {
-    name : 'Account'
-}
+  name: "Account",
+  data() {
+    return {
+      //regex for email validation
+      reg: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,24}))$/,
+      // item sets upload image default to null and user avater image
+      item: {
+        image: null,
+        imageUrl: "",
+      },
+    };
+  },
+  methods: {
+    //get user data from database
+    getUserData() {
+      var userObject;
+      var usernameField = document.getElementById("dname");
+      var emailField = document.getElementById("emailComp");
+      var passField = document.getElementById("password");
+      var confirmField = document.getElementById("confirm");
+      axios
+        .post("http://localhost:4000/account/get", {
+          userId: 4,
+        })
+        .then((response) => {
+          userObject = response.data["result"];
+          console.log(userObject);
+          usernameField.setAttribute("value", userObject[0].Username);
+          emailField.setAttribute("value", userObject[0].Email);
+          passField.setAttribute("value", userObject[0].Password);
+          confirmField.setAttribute("value", userObject[0].Password);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+
+    // send new username and email to database
+    setUserData(username, password) {
+      axios
+        .post("http://localhost:4000/account/set2", {
+          USERNAME: username,
+          PASSWORD: password,
+          USERID: 4,
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+
+    deleteUser() {},
+
+    // on change upload function to change picture from files
+    setDisplayPic(e) {
+      const file = e.target.files[0];
+      this.image = file;
+      this.item.imageUrl = URL.createObjectURL(file);
+    },
+    validate() {
+      //extract values
+      var username = String(document.getElementById("dname").value);
+      var password = String(document.getElementById("password").value);
+      var confirm = String(document.getElementById("confirm").value);
+      //var email = String(document.getElementById("emailComp").value);
+
+      var bValid = true;
+
+      // validating phone numbers-> if needed in future
+      // if (!this..match('^[0-9]{10}$')) { bValid=false }
+
+      /*       validation of email !NOT NEEDED!
+      var strRegex = new RegExp(
+        "^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$"
+      );
+      console.log(email.match(strRegex));
+      if (!this.reg.test(email)) {
+        bValid = false;
+        alert("Invalid e-mail");
+      } */
+
+      //validate username
+      if (username.length > 15 || username.length == 0) {
+        bValid = false;
+        alert("Invalid username length");
+      }
+
+      // validate password
+      if (password.length < 8 || password.length == 0) {
+        bValid = false;
+        alert("Password must be at least 8 characters");
+      }
+
+      // validate confirm password
+      if (password != confirm) {
+        bValid = false;
+        alert("Passwords do not match");
+      }
+
+      if (bValid) {
+        //add input to DB
+        try {
+          this.setUserData(username, password);
+        } catch (error) {
+          console.log(error);
+        }
+        console.log("User details captured successfully");
+        alert("User details captured successfully");
+      } else {
+        return;
+      }
+    },
+  }, // end of methods
+  mounted() {
+    this.getUserData();
+  },
+};
 </script>
 
 <template>
-    <div class="account-container d-flex flex-column">
-        <div class="row">
-            <h2>Account</h2>
+  <div class="account-container d-flex flex-column">
+    <div class="row">
+      <h2>Account</h2>
 
-            <div class="d-flex flex-column">
+      <div class="d-flex flex-column">
+        <h5 class="avatar-header">Avatar</h5>
+        <div id="preview" class="img-container d-flex">
+          <!-- upload button functionality -->
+          <img v-if="item.imageUrl" :src="item.imageUrl" class="user-avatar" />
+          <label class="upload-button">
+            <input
+              id="browse"
+              type="file"
+              accept="image/*"
+              style="hidden"
+              @change="setDisplayPic"
+            />
+            <i></i> Upload
+          </label>
+        </div>
+      </div>
+    </div>
+    <div id="invalidate">
+      <div class="row d-flex flex-row justify-content-between">
+        <div class="form-group">
+          <label for="dname">Username:</label>
 
-                <h5 class="avatar-header">
-                    Avatar
-                </h5>
-                    
-                <div class="img-container d-flex">
-
-                    <img class="user-avatar" src="../assets/defaultDP.png">
-                    <a class="upload-button">Upload</a>
-                    <a class="remove-button">Remove</a>
-        
-                </div>
-                    
-            </div>
+          <input
+            maxlength="15"
+            type="text"
+            class="form-control"
+            name="uname"
+            id="dname"
+            required
+          />
         </div>
 
-        <div class="row d-flex flex-row justify-content-between">
+        <div class="form-group">
+          <label for="emailComp">Email:</label>
+          <!-- changed it ot an email field for validation purposes- but not supported by all browsera
+                    therefore extra validation is added in validate method -->
+          <input
+            type="email"
+            class="form-control"
+            name="email"
+            id="emailComp"
+            disabled
+          />
+        </div>
+      </div>
 
-            <div class="form-group">
-
-                <label for="dname">Display Name:</label>
-                <input type="text" class="form-control" id="dname">
-            
-            </div>
-
-            <div class="form-group">
-
-                <label for="name">Full Name:</label>
-                <input type="password" class="form-control" id="name">
-
-            </div>
-            
+      <div class="row justify-content-between">
+        <div class="form-group">
+          <label for="password">Password:</label>
+          <input
+            type="password"
+            class="form-control"
+            id="password"
+            name="password"
+            required
+          />
         </div>
 
-        <div class="row d-flex flex-row justify-content-between">
-
-            <div class="form-group">
-
-                <label for="email">Email:</label>
-                <input type="text" class="form-control" id="email">
-            
-            </div>
-
-            <div class="form-group">
-
-                <label for="phone">Phone number:</label>
-                <input type="text" class="form-control" id="phone">
-
-            </div>
-
-            
+        <div class="form-group">
+          <label for="confirm">Confirm password:</label>
+          <input
+            type="password"
+            class="form-control"
+            name="confirm"
+            id="confirm"
+            required
+          />
         </div>
-
-        <div class="row d-flex flex-row justify-content-between">
-
-            <div class="form-group">
-
-                <label for="password">Password:</label>
-                <input type="text" class="form-control" id="password">
-            
-            </div>
-
-            <div class="form-group">
-
-                <label for="confirm">Confirm password:</label>
-                <input type="text" class="form-control" id="confirm">
-
-            </div>
-
-            
-        </div>
-
-        <div class="row d-flex">
-
-            <p> <strong>Delete account</strong> </p>
-            <div class="d-flex justify-content-between delete-account">
-                <p>By deleting your account you will lose all your data</p>
-                <a href="#">Delete account...</a>
-            </div>
-
-        </div>
-
-        <div class="row d-flex justify-content-between">
-
-            <a class="custom-btn" href="index.html">Back</a>
-
-            <a class="custom-btn">Save changes</a>
-
-        </div>
-
+      </div>
+    </div>
+    <div class="row d-flex">
+      <p><strong>Delete account</strong></p>
+      <div class="d-flex justify-content-between delete-account">
+        <p>By deleting your account you will lose all your data</p>
+        <a @click="deleteAccount" class="delete-btn">Delete</a>
+        <a @click="getUserData" class="custom-btn">Refresh</a>
+      </div>
     </div>
 
+    <div class="row d-flex justify-content-between">
+      <a @click="validate" class="custom-btn">Save changes</a>
+    </div>
+  </div>
 </template>
 
 <style scoped>
-.account-container{
-    width: 75%;
-    border-radius: 15px;
-    box-shadow: 0px 0px 14px -5px rgba(76, 75, 75, 0.75);
-    padding: 35px;
-    display: flex;
-    margin: 60px auto;
-
+.account-container {
+  width: 75%;
+  border-radius: 15px;
+  box-shadow: 0px 0px 14px -5px rgba(76, 75, 75, 0.75);
+  padding: 35px;
+  display: flex;
+  margin: 60px auto;
 }
 
-.account-container .row:not(:first-child), .account-container .row:not(:last-child){
-    margin-bottom: 20px;
-    padding-bottom: 25px;
-    border-bottom: 1px solid rgb(211, 211, 211);
-    
+.account-container .row:not(:first-child),
+.account-container .row:not(:last-child) {
+  margin-bottom: 20px;
+  padding-bottom: 25px;
+  border-bottom: 1px solid rgb(211, 211, 211);
 }
 
-.account-container .row:last-child{
-    border-bottom: none;
+.account-container .row:last-child {
+  border-bottom: none;
 }
 
-.account-container .row:first-child{
-    margin-bottom: 15px;
+.account-container .row:first-child {
+  margin-bottom: 15px;
 }
 
-.form-group label{
-    margin-bottom: 8px;
+.form-group label {
+  margin-bottom: 8px;
 }
 
-
-.form-group{
-    width: 440px;
+.form-group {
+  width: 440px;
 }
 
-.delete-account a{
-    text-decoration: none;
-    color: var(--bs-body-color);
+.delete-account a {
+  text-decoration: none;
+  color: var(--bs-body-color);
 }
 
-
-.avatar-header{
-    margin-top: 30px;
+.avatar-header {
+  margin-top: 30px;
 }
 
-.user-avatar{
-    width: 85px;
-    height: 85px;
-    border: 1px rgb(201, 204, 207) solid;
-    border-radius: 50%;
-    padding: 3px;
+.user-avatar {
+  width: 85px;
+  height: 85px;
+  border: 1px rgb(201, 204, 207) solid;
+  border-radius: 50%;
+  padding: 3px;
 }
 
-.img-container{
-    align-items: center;
+.img-container {
+  align-items: center;
 }
 
-.img-container a{
-    margin-left: 25px;
-    transition: .2s;
-
+.img-container a {
+  margin-left: 25px;
+  transition: 0.2s;
 }
 
-.upload-button, .remove-button{
-    border: 1px solid #ced4da;
-    width: 100px;
-    height: 40px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    text-decoration: none;
-    font-weight: 600;
-    border-radius: 4px;
+.upload-button,
+.remove-button {
+  border: 1px solid #ced4da;
+  width: 100px;
+  height: 40px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  text-decoration: none;
+  font-weight: 600;
+  border-radius: 4px;
+  margin-left: 20px;
 }
 
-.img-container a:hover{
-    border: 1px solid #0d6efd;
-    background: #0d6efd;
-    color: white;
+.img-container a:hover {
+  border: 1px solid #0d6efd;
+  background: #0d6efd;
+  color: white;
 }
 
-.custom-btn{
-    width: 160px;
-    height: 45px;
-    color: white;
-    font-weight: 700;
-    background: #0d6efd;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    border-radius: 6px;
-    text-decoration: none;
+.custom-btn {
+  width: 160px;
+  height: 45px;
+  color: white;
+  font-weight: 700;
+  background: #0d6efd;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 6px;
+  text-decoration: none;
+}
+
+.delete-btn {
+  width: 160px;
+  height: 45px;
+  color: white;
+  font-weight: 700;
+  background: #af0000;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 6px;
+  text-decoration: none;
+}
+
+/* makes input file type on upload button invisible */
+input[type="file"] {
+  display: none;
 }
 </style>
