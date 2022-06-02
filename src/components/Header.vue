da908421061fee0b5cef17bed7dc36acfcdfeaab
Tevin Padayachee <tevinp1@icloud.com>
2022-06-02 13:18:37 +0200

    fix for reload/logout bug

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -71,7 +71,7 @@ export default {
     <form class="form-inline d-flex">
 
         <input class="form-control mr-sm-2" id="myInput" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-primary my-2 my-sm-0" @click="filterFunction()">Search</button>
+        <button class="btn btn-outline-primary my-2 my-sm-0" type="button" @click="filterFunction()">Search</button>
 
     </form>
 
